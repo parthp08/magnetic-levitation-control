3496322a727ceea1f046665832c3384614095135
Parth Viradiya <36721553+parthp08@users.noreply.github.com>
2019-11-06 10:04:57 -0500

    Update Linearization.py

--- Linearization.py
+++ Linearization.py
@@ -57,19 +57,3 @@ def linearize(X_0):
 
     AA = 2*A
     return AA
-
-def print_matrix(matrix):
-    print("[")
-    for i in matrix:
-        print("[", end="")
-        for j in i:
-            print(f"{j:2f}\t", end="")
-        print("]")
-    print("]")
-
-def print_array(arr):
-    print("[")
-    for i in arr:
-        print(f"{i},")
-    print("]")
-
